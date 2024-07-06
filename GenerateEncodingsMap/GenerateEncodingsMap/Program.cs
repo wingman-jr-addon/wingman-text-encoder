5af79394b8332054447b0272fed287ac9c6b8e6d
Jesse Trana <jessenkaren@gmail.com>
2024-07-05 23:06:25 -0500

    Fixing UTF-8 charset on main index.html

--- GenerateEncodingsMap/GenerateEncodingsMap/Program.cs
+++ GenerateEncodingsMap/GenerateEncodingsMap/Program.cs
@@ -136,7 +136,7 @@ public static class Program
     {
         Directory.CreateDirectory(where);
         var builder = new StringBuilder();
-        builder.AppendLine("<!doctype html>\r\n<html> <head><meta http-equiv=\"Content-Type\" content=\"text/html;charset=\"utf-8\"> \r\n<style> iframe { \tborder: none; \tmargin: 0; \tpadding: 0; width: 100%; } </style></head> <body>\r\n");
+        builder.AppendLine("<!doctype html>\r\n<html> <head><meta http-equiv=\"Content-Type\" content=\"text/html;charset=utf-8\"> \r\n<style> iframe { \tborder: none; \tmargin: 0; \tpadding: 0; width: 100%; } </style></head> <body>\r\n");
         builder.AppendLine("<h1>Overview</h1>");
         builder.AppendLine("Tests show the UTF-8-based mapping on the first line, then test the encoding via a meta charset style declaration in an iframe following. The two should generally match.");
         builder.AppendLine("Known caveats: &lt; is skipped, apparent but OK mismatches will appear when the UTF-8 displays the codepoint in a box but the encoding displays only the diamond, and multi-character and UTF-16-based variants don't display the test correctly.");
